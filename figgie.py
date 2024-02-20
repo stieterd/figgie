0c2346921c95729d369465331f7ae4fd61a56e59
stieterd <44877194+stieterd@users.noreply.github.com>
2024-02-20 16:14:29 +0100

    final figgie version

--- figgie.py
+++ figgie.py
@@ -28,7 +28,7 @@ class TradingBot:
 
         except Exception as e:
             print(traceback.format_exc())
-            sys.exit()
+            raise Exception('error in parse_hand')
 
     @staticmethod
     def parse_orders(round_data):
@@ -40,32 +40,27 @@ class TradingBot:
                 offer_info = item[1]["offer"]
 
                 if bid_info != None:
-                    bid_dict = {"display_name": bid_info["user"]["display_name"], "price": bid_info["price"]}
+                    bid_dict = {"suit": item[0][0], "display_name": bid_info["user"]["display_name"], "price": bid_info["price"]}
                     bid_offer_prices["bid"].append(bid_dict)
 
                 if offer_info != None:
-                    offer_dict = {"display_name": offer_info["user"]["display_name"], "price": offer_info["price"]}
+                    offer_dict = {"suit": item[0][0], "display_name": offer_info["user"]["display_name"], "price": offer_info["price"]}
                     bid_offer_prices["offer"].append(offer_dict)
+
             return bid_offer_prices
         except Exception as e:
             print(traceback.format_exc())
-            sys.exit()
+            raise Exception('error in parse_orders')
 
     @staticmethod
     def on_trade_event(round_data: dict, round_history: dict) -> dict:
         '''Gets called on each trade event'''
         try:
-            ## Put your code here ##
-            current_orders = TradingBot.parse_orders(round_data)
-            hand = TradingBot.parse_hand(round_data)
-            print(current_orders)
-            print(hand)
-            # 0 means no actions, so if buy is 0 you are not buying
             return {'Diamonds': {'Buy': 0, 'Sell': 8}, 'Hearts': {'Buy': 4, 'Sell': 8}, 'Spades': {'Buy': 4, 'Sell': 4}, 'Clubs': {'Buy': 4, 'Sell': 0}}
 
         except Exception as e:
             print(traceback.format_exc())
-            sys.exit()
+            raise Exception('error in on_trade_event')
 
     @staticmethod
     def data_collection(round_data: dict, round_history: dict) -> bool:
@@ -80,7 +75,7 @@ class TradingBot:
             return False
         except Exception as e:
             print(traceback.format_exc())
-            sys.exit()
+            raise Exception('error in data_collection')
 
 
 class FiggieGame:
@@ -128,11 +123,11 @@ class FiggieGame:
         wsapp.send(json.dumps(
             ["Add_order", {"order": {"suit": [suit], "price": price, "direction": direction, "is_ioc": False}}]))
 
-    @staticmethod
-    def cancel_order(wsapp, direction: str, suit: str,
-                     price: int):  # direction: Buy or Sell, suit: Diamonds, Clubs, Spades, Hearts
-        wsapp.send(json.dumps(
-            ["Cancel_order", {"order": {"suit": [suit], "price": price, "direction": direction, "is_ioc": False}}]))
+    # @staticmethod
+    # def cancel_order(wsapp, direction: str, suit: str,
+    #                  price: int):  # direction: Buy or Sell, suit: Diamonds, Clubs, Spades, Hearts
+    #     wsapp.send(json.dumps(
+    #         ["Cancel_order", {"order": {"suit": [suit], "price": price, "direction": direction, "is_ioc": False}}]))
 
     def on_message(self, wsapp, message):
         message = json.loads(message)
@@ -156,38 +151,49 @@ class FiggieGame:
         elif "Live_round_for_player" in message[0]:
             round_data = message[1]['round']
             round_history = message[1]['round_history']
-            orders = TradingBot.on_trade_event(round_data, round_history)
-            for key, value in orders.items():
-                if len(value) == 0:
-                    continue
-                suit = key
 
-                for direction, price in value.items():
-                    veto = False
-                    for market in round_data['markets']:
-                        if price == 0:
-                            veto = True
-                            break
+            try:
+                orders = TradingBot.on_trade_event(round_data, round_history)
 
-                        if [suit] != market[0]:
-                            continue
+                for key, value in orders.items():
+                    if len(value) == 0:
+                        continue
+                    suit = key
 
-                        way = 'bid' if direction == "Buy" else 'offer'
-                        if market[1][way] is None:
-                            continue
+                    for direction, price in value.items():
+                        veto = False
+                        for market in round_data['markets']:
+                            if price == 0:
+                                veto = True
+                                break
 
-                        if direction == "Sell" and market[1][way]['user'][price] <= price:
-                            veto = True
-                        elif direction == "Buy" and market[1][way]['user'][price] >= price:
-                            veto = True
+                            if [suit] != market[0]:
+                                continue
 
-                    if not veto:
-                        self.add_order(wsapp, direction, suit, price)
+                            way = 'bid' if direction == "Buy" else 'offer'
+                            if market[1][way] is None:
+                                continue
+
+                            if direction == "Sell" and market[1][way]['price'] <= price:
+                                veto = True
+                            elif direction == "Buy" and market[1][way]['price'] >= price:
+                                veto = True
+
+                        if not veto:
+                            self.add_order(wsapp, direction, suit, price)
+            except Exception as e:
+                print(traceback.format_exc())
+                print(market[1][way]['user'])
+                sys.exit()
 
         elif "End_of_round_summary" in message[0]:
             round_data = message[1]['round']
             round_history = message[1]['round_history']
-            exit_term = TradingBot.data_collection(round_data, round_history)
+            try:
+                exit_term = TradingBot.data_collection(round_data, round_history)
+            except Exception as e:
+                print(traceback.format_exc())
+                sys.exit()
 
             if not exit_term:
                 self.start_next_round(wsapp)

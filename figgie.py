34b2cb3627b012c8109d4bfa15c14a3eb437df2f
stieterd <44877194+stieterd@users.noreply.github.com>
2024-02-21 13:52:59 +0100

    clean template

--- figgie.py
+++ figgie.py
@@ -13,9 +13,6 @@ import os
 import pandas
 import websocket
 import traceback
-import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
-import matplotlib.animation as animation
 
 from datetime import datetime
 import pandas as pd
@@ -44,43 +41,9 @@ class TradingBot:
 
         try:
 
-            current_orders = TradingBot.parse_current_orders(round_data)
-            hand = TradingBot.parse_current_hand(round_data)
+            ## Place your code here ##
 
-            result_bids = {'Diamonds': {}, 'Hearts': {},
-                           'Spades': {}, 'Clubs': {}}
-
-            # progress in percentages
-            game_progress = 1 - TradingBot.parse_game_time_left(round_data, round_history) / FiggieGame.ROUND_LENGTH_SECONDS
-            turning_point = 0.25
-
-            sell_price = max(15 - game_progress * 12, 5) if game_progress < 0.90 else 2
-            buy_price = 3
-
-            if game_progress > turning_point:
-                for key in result_bids.keys():
-                    result_bids[key]['Sell'] = sell_price
-
-            for key in result_bids.keys():
-                cur_buy_price = buy_price
-                for order in current_orders['offer']:
-                    if order['suit'] == key:
-                        if order['price'] <= 6 and order[
-                            'display_name'] != FiggieGame.DISPLAY_NAME and game_progress < turning_point:
-                            cur_buy_price = order['price']
-                            break
-
-                if game_progress < 0.80:
-                    if hand[key] < 5:
-                        result_bids[key]['Buy'] = cur_buy_price
-
-            # print(history_stats)
-            print(current_orders)
-            # print(hand)
-            print(result_bids)
-
-            # 0 means no actions, so if buy is 0 you are not buying
-            return result_bids  # {'Diamonds': {'Buy': 0, 'Sell': 8}, 'Hearts': {'Buy': 4, 'Sell': 8}, 'Spades': {'Buy': 4, 'Sell': 4}, 'Clubs': {'Buy': 4, 'Sell': 0}}
+            return {}
 
         except Exception as e:
             print(traceback.format_exc())
@@ -103,173 +66,14 @@ class TradingBot:
         '''
 
         try:
-            goal_suit = round_data['goal_suit'][0]
-            TradingBot.n_games += 1
 
-            TradingBot.draw_current_timeline(round_history)
-
-            if not os.path.isdir('figures'):
-                os.mkdir('figures')
-
-            plt.savefig(f'figures/{goal_suit}_{TradingBot.n_games}.png')
+            ## place your code here ##
 
             # return true if you want to terminate return false if you want to keep going
             return False
         except Exception as e:
             print(traceback.format_exc())
             raise Exception('error in data_collection')
-
-    @staticmethod
-    def parse_current_hand(round_data: dict) -> dict:
-        '''
-        :param round_data: dictionary containing the round data
-        :return: dictionary containing the cards in your hand
-
-        example: {"Hearts": 3, "Spades": 1}
-            if you have 3 hearts and 1 spade in your hand
-        '''
-        try:
-            for user in round_data['chips_and_hands']:
-                if user['user']['display_name'] == FiggieGame.DISPLAY_NAME:
-                    hand = {item[0][0]: item[1] for item in user['hand']}
-            return hand
-
-        except Exception as e:
-            print(traceback.format_exc())
-            raise Exception('error in parse_hand')
-
-    @staticmethod
-    def parse_current_orders(round_data: dict) -> dict:
-        '''
-        :param round_data: dictionary containing the round data
-        :return: dictionary containing orders currently on table with keys: offer and bid;
-            containing lists where each item is a dictionary with keys: suit, display_name, price
-
-        example: {"offer": [{'suit': 'Hearts', 'display_name': 'pjotr', 'price': 3}], "bid": []}
-            if pjotr placed a sell order of 3 on hearts
-        '''
-        try:
-            bid_offer_prices = {"offer": [], "bid": []}
-
-            for item in round_data['markets']:
-                bid_info = item[1]["bid"]
-                offer_info = item[1]["offer"]
-
-                if bid_info != None:
-                    bid_dict = {"suit": item[0][0], "display_name": bid_info["user"]["display_name"],
-                                "price": bid_info["price"]}
-                    bid_offer_prices["bid"].append(bid_dict)
-
-                if offer_info != None:
-                    offer_dict = {"suit": item[0][0], "display_name": offer_info["user"]["display_name"],
-                                  "price": offer_info["price"]}
-                    bid_offer_prices["offer"].append(offer_dict)
-
-            return bid_offer_prices
-        except Exception as e:
-            print(traceback.format_exc())
-            raise Exception('error in parse_orders')
-
-    @staticmethod
-    def parse_historical_orders(round_history: dict) -> pandas.DataFrame:
-        '''
-        :param round_history: dictionary containing all the trades and orders
-        :return: DataFrame with each order, the user who placed the order, the time of order, price, direction and card suit
-        '''
-
-
-        orders = pd.DataFrame(round_history['game_updates'], columns=['Type', 'Details'])
-        orders = orders[orders['Type'] == "Order"]
-        orders['Metadata'] = orders['Details'].apply(lambda x: x['metadata'])
-        orders['User'] = orders['Metadata'].apply(lambda x: x['user']['display_name'])
-        orders['Price'] = orders['Metadata'].apply(lambda x: x['price'])
-        orders['Time'] = pd.to_datetime(orders['Metadata'].apply(lambda x: x['time']))
-        orders['Suit'] = orders['Details'].apply(lambda x: x['suit'][0])
-        orders['Direction'] = orders['Details'].apply(lambda x: x['direction'])
-        orders.drop(columns=['Details', 'Metadata'], inplace=True)
-
-        return orders
-
-    @staticmethod
-    def parse_historical_trades(round_history: dict) -> pandas.DataFrame:
-        '''
-        :param round_history: dictionary containing all the trades and orders
-        :return: DataFrame with each trade, the seller, the buyer, the time of trade, price, direction and card suit
-        '''
-
-
-        trades = pd.DataFrame(round_history['trades'], columns=['Type', 'Details'])
-        trades = trades[trades['Type'] == "Trade"]
-        # trades['Metadata'] = trades['Details'].apply(lambda x: x['metadata'])
-        trades['Seller'] = trades['Details'].apply(lambda x: x['seller']['display_name'])
-        trades['Buyer'] = trades['Details'].apply(lambda x: x['buyer']['display_name'])
-        trades['Price'] = trades['Details'].apply(lambda x: x['price'])
-        trades['Time'] = pd.to_datetime(trades['Details'].apply(lambda x: x['time']))
-        trades['Suit'] = trades['Details'].apply(lambda x: x['suit'][0])
-        trades['Direction'] = trades['Details'].apply(lambda x: x['direction'])
-        trades.drop(columns=['Details'], inplace=True)
-
-        return trades
-
-    @staticmethod
-    def parse_game_time_left(round_data: dict, round_history: dict):
-
-        '''
-        :param round_data: dictionary containing the round data
-        :param round_history: dictionary containing all the trades and orders
-        :return: float value with the time left until game finished
-        '''
-
-        if len(round_history['game_updates']) == 0:
-            return 240
-
-        if round_history['game_updates'][0][0] == 'Trade':
-            result = datetime.strptime(round_data['end_time'].split('.')[0], '%Y-%m-%d %H:%M:%S') - datetime.strptime(
-                round_history['game_updates'][0][1]['time'].split('.')[0], '%Y-%m-%d %H:%M:%S')
-        else:
-            result = datetime.strptime(round_data['end_time'].split('.')[0],
-                                       '%Y-%m-%d %H:%M:%S') - datetime.strptime(
-                round_history['game_updates'][0][1]['metadata']['time'].split('.')[0], '%Y-%m-%d %H:%M:%S')
-
-        return result.total_seconds()
-
-    @staticmethod
-    def draw_current_timeline(round_history):
-        '''
-        :param round_history: dictionary containing all the trades and orders
-
-        creates a plot with 4 figures for each card type
-        with green line representing the trades done
-        with red line representing the sell orders
-        with blue line representing the buy orders
-        '''
-
-        colors = ['b-', 'r-']
-
-        orders = TradingBot.parse_historical_orders(round_history)
-        trades = TradingBot.parse_historical_trades(round_history)
-
-        plt.clf()
-
-        for pos, suit in enumerate(sorted(orders['Suit'].unique())):
-            for idx, direction in enumerate(sorted(orders['Direction'].unique())):
-                temp_df = orders[(orders['Direction'] == direction) & (orders['Suit'] == suit)]
-                plt.subplot(2, 2, 1 + pos)
-                plt.plot(temp_df['Time'], temp_df['Price'], colors[idx], marker=None, label=direction)
-
-            temp_trades = trades[trades['Suit'] == suit]
-            plt.subplot(2, 2, 1 + pos)
-            plt.plot(temp_trades['Time'], temp_trades['Price'], 'go-')
-
-            plt.title(suit)
-            plt.xlabel('Time')
-            plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%H:%M:%S'))
-            plt.xticks([orders['Time'].min(), orders['Time'].max()], rotation=45)
-            plt.ylim(0, 30)
-            plt.ylabel('Price')
-
-        plt.tight_layout()
-
 
 class FiggieGame:
     NBOTS = 4
